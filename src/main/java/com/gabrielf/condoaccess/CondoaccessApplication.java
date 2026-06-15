8083511eb9affdc6c9ef0e83d94f8666a0e723d3
gbzadaf <fonsecagb23@gmail.com>
2026-06-14 21:45:46 -0300

    First commit

---
+++ src/main/java/com/gabrielf/condoaccess/CondoaccessApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielf.condoaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondoaccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondoaccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielf/condoaccess/CondoaccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielf.condoaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CondoaccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
